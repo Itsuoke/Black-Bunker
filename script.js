1aa7b4b45d4fe03a396dd3eb843f3018e4c045e8
Itsuoke <douglasricardo033@gmail.com>
2025-09-21 18:22:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function sim() {
+    alert("Você aceitou casar com o Hells! <3")
+}
+
+
+function desvia(t) {
+    var btn = t;
+    btn.style.position = 'absolute'
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log("Opa desviei neném hehe!! (^w^)z")
+}
+
+
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
+
